ad26fd7af9be8ae30363d55943ae97661aedb90a
Студент <student@comp-4-306-08.corp.omsu.ru>
2026-09-05 13:25:31 +0600

    Добавлены макросы для ID предметов

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #define ARR_SIZE 10
+#define EMPTY 0
+#define WOOD 1
+#define STONE 2
+#define SEEDS 3
+#define GRASS 4
+#define CLAY 5
+#define LEAF 6
+#define COPPER 7
+#define IRON 8
+#define GOLD 9
+#define CARBON 10
+
 void main()
 {
 	int current_day = 1;
 	int current_hour = 8;
 	
-	int inventory[10] = {0};
+	int inventory[ARR_SIZE] = {0};
 }
